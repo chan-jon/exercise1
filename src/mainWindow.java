4a79a8292a640eb4db28e97294f835fe5d2e9cf9
Jon <jonathan.chan4@gmail.com>
2017-10-08 19:17:36 -0400

    Added image to buttonC as a test for now
    Signed-off-by: Jon <jonathan.chan4@gmail.com>

--- src/mainWindow.java
+++ src/mainWindow.java
@@ -1,3 +1,4 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 
@@ -8,8 +9,12 @@ public class mainWindow extends JFrame {
     private JButton buttonBButton;
     private JButton buttonAButton;
 
+    Icon imgC = new ImageIcon("images/pen.png");
+
+
     public mainWindow() {
         setContentPane(panel1);
+        buttonCButton.setIcon(imgC);
         pack();
         setVisible(true);
     }
